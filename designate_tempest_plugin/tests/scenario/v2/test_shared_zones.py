e9a47fc82efd29b18a98f890a904fadc781576c9
Mikhail Samoylov <mikhailsamoiloff@gmail.com>
2025-03-04 10:12:13 +0400

    Setup credentials for shared zones test

--- designate_tempest_plugin/tests/scenario/v2/test_shared_zones.py
+++ designate_tempest_plugin/tests/scenario/v2/test_shared_zones.py
@@ -39,6 +39,7 @@ class SharedZonesTest(base.BaseDnsV2Test):
     @classmethod
     def setup_clients(cls):
         super(SharedZonesTest, cls).setup_clients()
+        cls.zones_client = cls.os_primary.zones_client
         cls.admin_tld_client = cls.os_admin.tld_client
         cls.adm_shr_client = cls.os_admin.shared_zones_client
         cls.share_zone_client = cls.os_primary.shared_zones_client
