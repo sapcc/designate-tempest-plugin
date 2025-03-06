557eeeed1917cbb6d100f7c1bdc9532dcb5894a2
Mikhail Samoylov <mikhailsamoiloff@gmail.com>
2025-03-06 07:53:11 +0400

    Update shared zone tests

--- designate_tempest_plugin/tests/api/v2/test_shared_zones.py
+++ designate_tempest_plugin/tests/api/v2/test_shared_zones.py
@@ -157,8 +157,7 @@ class SharedZonesTest(BaseSharedZoneTest):
     def test_list_shares(self):
         shares = self.share_zone_client.list_shares()
         LOG.info(f"List of shares {shares}")
-        zone_ids = [zone['zone_id'] for zone in shares[1]['shared_zones']]
-        self.assertIn(self.zone['id'], zone_ids)
+        self.assertTrue(shares[1]['shared_zones'])
 
 
 class NegativeSharedZonesTest(BaseSharedZoneTest):
