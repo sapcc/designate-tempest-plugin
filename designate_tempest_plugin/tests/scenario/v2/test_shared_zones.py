81448245edf47e91e83432d984c4ea02c2c0d440
Mikhail Samoylov <mikhailsamoiloff@gmail.com>
2025-04-02 10:14:56 +0400

    Update shared zones test

--- designate_tempest_plugin/tests/scenario/v2/test_shared_zones.py
+++ designate_tempest_plugin/tests/scenario/v2/test_shared_zones.py
@@ -349,6 +349,8 @@ class SharedZonesTest(base.BaseDnsV2Test):
         LOG.info(f"Demo client project id {self.demo_rec_client.project_id}")
         LOG.info(f"Alt client project id {self.alt_rec_client.project_id}")
         LOG.info(f"Share zone client project id {self.share_zone_client.project_id}")
+        LOG.info(f"Zone client project id {self.zones_client.project_id}")
+        LOG.info(f"Admin share zone client project id {self.adm_shr_client.project_id}")
         shared_zone = self.share_zone_client.create_zone_share(
             zone['id'], self.demo_rec_client.project_id)[1]
         self.addCleanup(self.share_zone_client.delete_zone_share,
